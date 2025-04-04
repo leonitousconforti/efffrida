79f725ce491bdf6e9ebcf57627163d4e8c6c2935
Leo Conforti <leoconforti@ltgk.net>
2025-04-04 20:25:43 +0000

    delete

--- packages/effect-platform-frida/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    entry: ["test/playground.ts"],
-    clean: true,
-    publicDir: true,
-    treeshake: "smallest",
-    noExternal: [/(.*)/],
-});
