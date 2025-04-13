a257d15c03e8f7663e2b5893c11f3a7f676aec13
Leo Conforti <leoconforti@ltgk.net>
2025-04-13 19:54:44 +0000

    save

--- packages/rpc/test/rpc.test.ts
+++ packages/rpc/test/rpc.test.ts
@@ -5,7 +5,7 @@ import { RpcClient, RpcSerialization } from "@effect/rpc";
 import { FridaCompile } from "@efffrida/frida-compile";
 import { FridaDevice, FridaScript, FridaSession } from "@efffrida/frida-tools";
 import { FridaRpcClient } from "@efffrida/rpc";
-import { Chunk, Console, Effect, Layer, Option, Stream } from "effect";
+import { Chunk, Effect, Layer, Option, Stream } from "effect";
 import { ScriptRuntime } from "frida";
 import { UserRpcs } from "../shared/requests.js";
 
@@ -18,12 +18,13 @@ const ProtocolLive = FridaRpcClient.layerProtocolFrida().pipe(Layer.provide([NdJ
 // Pick a device and a session/program
 const DeviceLive = FridaDevice.layerLocalDevice;
 const SessionLive = FridaSession.layer("/usr/bin/sleep");
+const FridaLive = Layer.provideMerge(SessionLive, DeviceLive);
 
 // Compile the agent
 const ScriptLive = FridaCompile.compileAgent(new URL("../frida/agent.ts", import.meta.url))
     .pipe(Effect.map(FridaScript.layer({ runtime: ScriptRuntime.V8 })))
     .pipe(Layer.unwrapEffect)
-    .pipe(Layer.provide([Layer.provideMerge(SessionLive, DeviceLive), NodeContext.layer]));
+    .pipe(Layer.provide([FridaLive, NodeContext.layer]));
 
 // Now we have an rpc client layer with no dependencies
 const Live = Layer.provide(ProtocolLive, ScriptLive);
@@ -40,8 +41,6 @@ describe("Should be able to perform rpc communication", () => {
             if (Option.isNone(Chunk.findFirst(users, (user) => user.id === "3"))) {
                 yield* client.UserCreate({ name: "Charlie" });
                 users = yield* Stream.runCollect(client.UserList());
-            } else {
-                yield* Console.log(`User "Charlie" already exists`);
             }
             expect(users).toEqual(
                 Chunk.make({ id: "1", name: "Alice" }, { id: "2", name: "Bob" }, { id: "3", name: "Charlie" })
