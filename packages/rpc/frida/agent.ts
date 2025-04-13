5a26e439d96f546ab1e3d30ba8b72caf058e54f9
Leo Conforti <leoconforti@ltgk.net>
2025-04-13 18:43:34 +0000

    save agent

--- packages/rpc/frida/agent.ts
+++ packages/rpc/frida/agent.ts
@@ -1,24 +1,18 @@
-import * as A from "effect/Effect";
-
-console.log(A);
-
-// import { FridaRpcServer } from "@efffrida/rpc";
-// import { UserRpcs } from "../shared/requests.js";
-// import { UsersLive } from "./handlers.js";
+import { RpcSerialization, RpcServer } from "@effect/rpc";
+import { layerProtocolFrida } from "@efffrida/rpc/FridaRpcServer";
+import { Effect, Layer } from "effect";
+import { UserRpcs } from "../shared/requests.js";
+import { UsersLive } from "./handlers.js";
 
 // Create the RPC server layer
-// const RpcLayer = RpcServer.layer(UserRpcs).pipe(Layer.provide(UsersLive));
+const RpcLayer = RpcServer.layer(UserRpcs).pipe(Layer.provide(UsersLive));
 
-// // Choose the protocol and serialization format
-// const NdJsonSerialization = RpcSerialization.layerNdjson;
-// const FridaProtocol = FridaRpcServer.layerProtocolFrida({ onRpcAvailable: "here" }).pipe(
-//     Layer.provide(NdJsonSerialization)
-// );
+// Choose the protocol and serialization format
+const NdJsonSerialization = RpcSerialization.layerNdjson;
+const FridaProtocol = layerProtocolFrida({ onRpcAvailable: "here" }).pipe(Layer.provide(NdJsonSerialization));
 
-// // Create the main rpc layer
-// const Main = RpcLayer.pipe(Layer.provide(FridaProtocol));
+// Create the main rpc layer
+const Main = RpcLayer.pipe(Layer.provide(FridaProtocol));
 
-// // Start the server
-// Effect.runFork(Layer.launch(Main));
-
-send("here");
+// Start the server
+Effect.runFork(Layer.launch(Main));
