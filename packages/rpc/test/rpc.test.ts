96403845f7b845d3d812b116caedf2ca06501a0f
Leo Conforti <leoconforti@ltgk.net>
2025-04-13 19:34:50 +0000

    thats a passing test case

--- packages/rpc/test/rpc.test.ts
+++ packages/rpc/test/rpc.test.ts
@@ -1,51 +1,54 @@
-import { describe, it } from "@effect/vitest";
+import { describe, expect, it } from "@effect/vitest";
 
 import { NodeContext } from "@effect/platform-node";
 import { RpcClient, RpcSerialization } from "@effect/rpc";
 import { FridaCompile } from "@efffrida/frida-compile";
 import { FridaDevice, FridaScript, FridaSession } from "@efffrida/frida-tools";
 import { FridaRpcClient } from "@efffrida/rpc";
-import { Chunk, Console, Effect, Layer, Stream } from "effect";
+import { Chunk, Console, Effect, Layer, Option, Stream } from "effect";
 import { ScriptRuntime } from "frida";
 import { UserRpcs } from "../shared/requests.js";
 
-// Choose which protocol to use
+// Choose which serialization to use
 const NdJsonSerialization = RpcSerialization.layerNdjson;
+
+// Choose which protocol to use
 const ProtocolLive = FridaRpcClient.layerProtocolFrida().pipe(Layer.provide([NdJsonSerialization]));
 
 // Pick a device and a session/program
-const LocalDeviceLive = Layer.provideMerge(FridaSession.layer("/usr/bin/sleep"), FridaDevice.layerLocalDevice);
+const DeviceLive = FridaDevice.layerLocalDevice;
+const SessionLive = FridaSession.layer("/usr/bin/sleep");
 
 // Compile the agent
 const ScriptLive = FridaCompile.compileAgent(new URL("../frida/agent.ts", import.meta.url))
     .pipe(Effect.map(FridaScript.layer({ runtime: ScriptRuntime.V8 })))
     .pipe(Layer.unwrapEffect)
-    .pipe(Layer.provide(LocalDeviceLive))
-    .pipe(Layer.provide(NodeContext.layer));
+    .pipe(Layer.provide([Layer.provideMerge(SessionLive, DeviceLive), NodeContext.layer]));
 
 // Now we have an rpc client layer with no dependencies
-const RpcLive = Layer.provideMerge(ProtocolLive, ScriptLive);
+const Live = Layer.provide(ProtocolLive, ScriptLive);
 
 // Use the client
 describe("Should be able to perform rpc communication", () => {
-    it.effect("demo from rpc readme", () =>
+    it.scoped("demo from rpc readme", () =>
         Effect.gen(function* () {
             const client = yield* RpcClient.make(UserRpcs);
             const user = yield* client.UserById({ id: "1" });
-            yield* Console.log("User: ", user);
-            let users = yield* Stream.runCollect(client.UserList({}));
-            if (!Chunk.findFirst(users, (user) => user.id === "3")) {
-                yield* Console.log(`Creating user "Charlie"`);
+            expect(user).toEqual({ id: "1", name: "Alice" });
+            // yield* Console.log("User: ", user);
+            let users = yield* Stream.runCollect(client.UserList());
+            // yield* Console.log("Users: ", users);
+            if (Option.isNone(Chunk.findFirst(users, (user) => user.id === "3"))) {
+                // yield* Console.log(`Creating user "Charlie"`);
                 yield* client.UserCreate({ name: "Charlie" });
                 users = yield* Stream.runCollect(client.UserList({}));
             } else {
                 yield* Console.log(`User "Charlie" already exists`);
             }
-            yield* Console.log("Users: ", users);
-        })
-            .pipe(Effect.scoped)
-            .pipe(Effect.provide(RpcLive))
-            .pipe(Effect.provide(LocalDeviceLive))
-            .pipe(Effect.provide(NodeContext.layer))
+            // yield* Console.log("Users: ", users);
+            expect(users).toEqual(
+                Chunk.make({ id: "1", name: "Alice" }, { id: "2", name: "Bob" }, { id: "3", name: "Charlie" })
+            );
+        }).pipe(Effect.provide(Live))
     );
 });
