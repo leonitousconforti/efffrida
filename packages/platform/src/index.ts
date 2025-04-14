a7e3e422e7540c1c2c35c174cefc29c9c23b4127
Leo Conforti <leoconforti@ltgk.net>
2025-04-14 05:58:47 +0000

    codegen

---
+++ packages/platform/src/index.ts
@@ -0,0 +1,34 @@
+/**
+ * Effect `Channel` utilities for Frida.
+ *
+ * @since 1.0.0
+ */
+export * as Channel from "./Channel.js"
+
+/**
+ * Frida `Memory Access Monitor` utilities for Effect.
+ *
+ * @since 1.0.0
+ */
+export * as MemoryAccessMonitor from "./MemoryAccessMonitor.js"
+
+/**
+ * Effect `Sink` utilities for Frida.
+ *
+ * @since 1.0.0
+ */
+export * as Sink from "./Sink.js"
+
+/**
+ * Effect `Socket` utilities for Frida.
+ *
+ * @since 1.0.0
+ */
+export * as Socket from "./Socket.js"
+
+/**
+ * Effect `Stream` utilities for Frida.
+ *
+ * @since 1.0.0
+ */
+export * as Stream from "./Stream.js"
