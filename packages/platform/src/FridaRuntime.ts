7365c5559323c760384dd5f6847d4d0370f75a2b
Leo Conforti <leoconforti@ltgk.net>
2025-04-22 14:41:23 +0000

    update frida runtime

--- packages/platform/src/FridaRuntime.ts
+++ packages/platform/src/FridaRuntime.ts
@@ -4,30 +4,62 @@
  * @since 1.0.0
  */
 
+import type * as Exit from "effect/Exit";
+import type * as FiberId from "effect/FiberId";
+
 import * as Cause from "effect/Cause";
 import * as Effect from "effect/Effect";
+import * as FiberRef from "effect/FiberRef";
+import * as FiberRefs from "effect/FiberRefs";
+import * as Function from "effect/Function";
+import * as HashSet from "effect/HashSet";
+import * as Logger from "effect/Logger";
+import * as Predicate from "effect/Predicate";
 
 /**
  * @since 1.0.0
  * @category Runtime
  */
-export const runMain = <A, E>(effect: Effect.Effect<A, E, never>): void => {
+export const runMain = <A, E>(
+    effect: Effect.Effect<A, E, never>,
+    options?:
+        | {
+              readonly disablePrettyLogger?: boolean | undefined;
+              readonly teardown?: ((exit: Exit.Exit<A, E>) => void) | undefined;
+          }
+        | undefined
+): void => {
     const throwNextTick = (throwable: unknown): void =>
-        Script.nextTick((error) => {
+        Script.nextTick((error): never => {
             throw error;
         }, throwable);
 
-    Effect.runFork(
-        Effect.tapErrorCause(effect, (cause) => {
-            if (Cause.isInterrupted(cause)) return Effect.void;
-            // const error = Cause.failureOrCause(cause);
-            // if (error._tag === "Left") {
-            //     throwNextTick(error.left);
-            // } else {
-            //     throwNextTick(error.right);
-            // }
-            throwNextTick(cause);
+    const throwFromTapErrorCause: <A1, E1, R1>(self: Effect.Effect<A1, E1, R1>) => Effect.Effect<A1, E1, R1> =
+        Effect.tapErrorCause((cause): Effect.Effect<void, never, never> => {
+            if (!Cause.isInterruptedOnly(cause)) {
+                throwNextTick(cause);
+                return Function.absurd(cause as never);
+            }
             return Effect.void;
-        })
+        });
+
+    const addPrettyLogger = (refs: FiberRefs.FiberRefs, fiberId: FiberId.Runtime): FiberRefs.FiberRefs => {
+        const loggers = FiberRefs.getOrDefault(refs, FiberRef.currentLoggers);
+        if (!HashSet.has(loggers, Logger.defaultLogger)) {
+            return refs;
+        }
+        return FiberRefs.updateAs(refs, {
+            fiberId,
+            fiberRef: FiberRef.currentLoggers,
+            value: loggers.pipe(HashSet.remove(Logger.defaultLogger), HashSet.add(Logger.prettyLoggerDefault)),
+        });
+    };
+
+    const fiber = Effect.runFork(throwFromTapErrorCause(effect), {
+        updateRefs: options?.disablePrettyLogger === true ? undefined : addPrettyLogger,
+    } as const);
+
+    fiber.addObserver((exit: Exit.Exit<A, E>): void =>
+        Predicate.isNotUndefined(options?.teardown) ? options.teardown(exit) : void 0
     );
 };
