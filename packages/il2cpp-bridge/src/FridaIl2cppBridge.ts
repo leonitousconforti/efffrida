3afd42093b612db4d24f88105854af51e64d3fe1
Leo Conforti <leoconforti@ltgk.net>
2025-04-25 12:53:17 +0000

    start il2cpp bridge

--- packages/il2cpp-bridge/src/FridaIl2cppBridge.ts
+++ packages/il2cpp-bridge/src/FridaIl2cppBridge.ts
@@ -4,5 +4,35 @@
  * @since 1.0.0
  */
 
-/** @since 1.0.0 */
-export const A = "A";
+import "frida-il2cpp-bridge";
+
+import * as Effect from "effect/Effect";
+import * as Exit from "effect/Exit";
+import * as Runtime from "effect/Runtime";
+
+/**
+ * Attaches the caller thread to Il2Cpp domain and executes the given block.
+ *
+ * @since 1.0.0
+ */
+export const il2cppPerformEffect: <X, E, R>(
+    effect: Effect.Effect<X, E, R>,
+    flag?: "free" | "bind" | "leak" | "main" | undefined
+) => Effect.Effect<void, E, R> = Effect.fnUntraced(function* <X, E, R>(
+    effect: Effect.Effect<X, E, R>,
+    flag?: "free" | "bind" | "leak" | "main" | undefined
+) {
+    const runtime = yield* Effect.runtime<R>();
+    const runPromiseExit = Runtime.runPromiseExit(runtime);
+    const exit = yield* Effect.promise((abortSignal) =>
+        Il2Cpp.perform(
+            () =>
+                runPromiseExit(effect, {
+                    signal: abortSignal,
+                }),
+            flag
+        )
+    );
+    const voided = Exit.asVoid(exit);
+    return yield* voided;
+});
