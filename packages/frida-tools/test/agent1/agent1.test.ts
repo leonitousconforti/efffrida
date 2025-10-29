bba9c7b9bbcfe3493cabe5f7cad5ff7e0a2e92c6
Leo Conforti <leo@leoconforti.us>
2025-10-28 21:24:54 -0500

    try this

--- packages/frida-tools/test/agent1/agent1.test.ts
+++ packages/frida-tools/test/agent1/agent1.test.ts
@@ -8,17 +8,22 @@ const ScriptLive = FridaScript.layer(new URL("main.agent.ts", import.meta.url));
 
 layer(FridaLive)("local device tests", (it) => {
     it.layer(ScriptLive)((it) => {
-        it.scoped("agent1 on local device", () =>
-            Effect.gen(function* () {
-                const script = yield* FridaScript.FridaScript;
-                const firstMessage = yield* script.stream.pipe(Stream.runHead);
-                expect(firstMessage).toStrictEqual(
-                    Option.some({
-                        message: "Hello from Frida0!",
-                        data: Option.none(),
-                    })
-                );
-            })
+        it.scoped(
+            "agent1 on local device",
+            () =>
+                Effect.gen(function* () {
+                    const script = yield* FridaScript.FridaScript;
+                    const firstMessage = yield* script.stream.pipe(Stream.runHead);
+                    expect(firstMessage).toStrictEqual(
+                        Option.some({
+                            message: "Hello from Frida0!",
+                            data: Option.none(),
+                        })
+                    );
+                }),
+            {
+                timeout: 20_000,
+            }
         );
     });
 });
