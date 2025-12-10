52cd6b64cb0deb8968f799e970e1bff16ea3ad8d
Leo Conforti <leo@leoconforti.us>
2025-12-10 11:45:17 -0600

    try timeout

--- packages/frida-tools/test/agent4/agent4.test.ts
+++ packages/frida-tools/test/agent4/agent4.test.ts
@@ -6,8 +6,8 @@ import { FridaLive } from "../base.ts";
 
 const ScriptLive = FridaScript.layer(new URL("effect.agent.ts", import.meta.url));
 
-layer(FridaLive, { excludeTestServices: true, timeout: "1 minute" })("local device tests", (it) => {
-    it.layer(ScriptLive)((it) => {
+layer(FridaLive, { excludeTestServices: true })("local device tests", (it) => {
+    it.layer(ScriptLive, { timeout: "1 minute" })((it) => {
         it.scoped("agent4 on local device", () =>
             Effect.gen(function* () {
                 const script = yield* FridaScript.FridaScript;
