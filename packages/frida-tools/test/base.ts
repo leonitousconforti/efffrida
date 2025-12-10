6638d340b1a79befb9c50429474f1d6142248e67
Leo Conforti <leo@leoconforti.us>
2025-12-10 12:23:16 -0600

    save

--- packages/frida-tools/test/base.ts
+++ packages/frida-tools/test/base.ts
@@ -1,27 +1,63 @@
-import { FridaDevice, FridaSession } from "@efffrida/frida-tools";
-import { Effect, Layer, Scope } from "effect";
-import { spawn } from "node:child_process";
+import { Command, CommandExecutor } from "@effect/platform";
+import { NodeContext } from "@effect/platform-node";
+import { FridaDevice, FridaSession, FridaSessionError } from "@efffrida/frida-tools";
+import { Effect, Layer } from "effect";
+
+import * as Frida from "frida";
 
 // Pick a device and a session/program
 export const DeviceLive = FridaDevice.layerLocalDevice;
 export const SessionLive = Layer.unwrapScoped(
     Effect.gen(function* () {
-        const scope = yield* Scope.Scope;
+        const executor = yield* CommandExecutor.CommandExecutor;
+        const command = Command.make("sleep", "infinity");
+        const process = yield* executor.start(command);
 
-        // Use raw Node.js child_process to spawn
-        const child = spawn("sleep", ["infinity"], {
-            detached: false,
-            stdio: "ignore",
+        const session = yield* Effect.tryPromise({
+            try: (signal) => {
+                const cancellable = new Frida.Cancellable();
+                signal.onabort = () => cancellable.cancel();
+                return Frida.attach(process.pid, {}, cancellable);
+            },
+            catch: (cause) =>
+                new FridaSessionError.FridaSessionError({
+                    cause,
+                    when: "attach",
+                }),
         });
 
-        // Kill the process when scope closes
-        yield* Scope.addFinalizer(
-            scope,
-            Effect.sync(() => child.kill())
-        );
-
-        return FridaSession.layer(child.pid!);
+        return Layer.succeed(FridaSession.FridaSession, {
+            session,
+            resume: Effect.tryPromise((signal) => {
+                const cancellable = new Frida.Cancellable();
+                signal.onabort = () => cancellable.cancel();
+                return session.resume(cancellable);
+            }),
+            enableChildGating: Effect.tryPromise((signal) => {
+                const cancellable = new Frida.Cancellable();
+                signal.onabort = () => cancellable.cancel();
+                return session.enableChildGating(cancellable);
+            }),
+            disableChildGating: Effect.tryPromise((signal) => {
+                const cancellable = new Frida.Cancellable();
+                signal.onabort = () => cancellable.cancel();
+                return session.disableChildGating(cancellable);
+            }),
+            setupPeerConnection: (opts?: Frida.PeerOptions | undefined) =>
+                Effect.tryPromise((signal) => {
+                    const cancellable = new Frida.Cancellable();
+                    signal.onabort = () => cancellable.cancel();
+                    return session.setupPeerConnection(opts, cancellable);
+                }),
+            joinPortal: (address: string, opts?: Frida.PortalOptions | undefined) =>
+                Effect.tryPromise((signal) => {
+                    const cancellable = new Frida.Cancellable();
+                    signal.onabort = () => cancellable.cancel();
+                    return session.joinPortal(address, opts, cancellable);
+                }),
+            [FridaSession.FridaSessionTypeId]: FridaSession.FridaSessionTypeId,
+        });
     })
 );
 
-export const FridaLive = Layer.provide(SessionLive, DeviceLive);
+export const FridaLive = Layer.provide(SessionLive, Layer.merge(DeviceLive, NodeContext.layer));
