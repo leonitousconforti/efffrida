21ab5c2b3e8a6a89d5a88d55d3c76514f82a0a24
Leo Conforti <leo@leoconforti.us>
2025-12-10 00:07:35 -0600

    test2

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        projects: ["packages/frida-tools/vitest.config.ts"],
+        projects: ["packages/frida-tools/vitest.config.ts", "packages/vitest-pool/vitest.config.ts"],
     },
 });
