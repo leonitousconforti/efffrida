7aef852a4a126003838460da5432b290c375344d
Leo Conforti <leo@leoconforti.us>
2025-12-09 22:35:14 -0600

    fix rpc tests

--- packages/rpc/test/frida/client/agent.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("RPC Client Tests", () => {
-    it("should establish a connection", async () => {
-        expect(true).toBe(true);
-    });
-});
