11d1083ee526270fa34b92593eeb8f27335248c2
Leo Conforti <leo@leoconforti.us>
2025-12-10 11:47:40 -0600

    test

--- packages/frida-tools/test/agent4/agent4.test.ts
+++ packages/frida-tools/test/agent4/agent4.test.ts
@@ -7,7 +7,7 @@ import { FridaLive } from "../base.ts";
 const ScriptLive = FridaScript.layer(new URL("effect.agent.ts", import.meta.url));
 
 layer(FridaLive, { excludeTestServices: true })("local device tests", (it) => {
-    it.layer(ScriptLive, { timeout: "1 minute" })((it) => {
+    it.layer(ScriptLive)((it) => {
         it.scoped("agent4 on local device", () =>
             Effect.gen(function* () {
                 const script = yield* FridaScript.FridaScript;
