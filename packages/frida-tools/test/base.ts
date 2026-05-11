750d1ad812c99da5426342399481a36eaa694bb0
Leo Conforti <leo@leoconforti.us>
2026-05-11 16:33:16 -0500

    simplify testing

--- packages/frida-tools/test/base.ts
+++ packages/frida-tools/test/base.ts
@@ -1,28 +1,8 @@
-import { Command, CommandExecutor } from "@effect/platform";
 import { NodeContext } from "@effect/platform-node";
 import { FridaDevice, FridaSession } from "@efffrida/frida-tools";
-import { Duration, Effect, Layer, Schedule } from "effect";
+import { Layer } from "effect";
 
 // Pick a device and a session/program
 export const DeviceLive = FridaDevice.layerLocalDevice;
-
-// For nix 😍
-export const SessionLive = Layer.unwrapScoped(
-    Effect.gen(function* () {
-        const executor = yield* CommandExecutor.CommandExecutor;
-        const command = Command.make("sleep", "infinity");
-        const proc = yield* executor.start(command);
-
-        // For GitHub Actions 🤮
-        if (process.env.CI !== undefined) {
-            return Layer.retry(
-                FridaSession.layer(proc.pid),
-                Schedule.addDelay(Schedule.recurs(2), () => Duration.seconds(1))
-            );
-        }
-
-        return FridaSession.layer(proc.pid);
-    })
-);
-
+export const SessionLive = FridaSession.layer(["/usr/bin/sleep", "infinity"]);
 export const FridaLive = Layer.provide(SessionLive, Layer.merge(DeviceLive, NodeContext.layer));
