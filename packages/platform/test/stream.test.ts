64abffc53c1beef16612ff9f96ab420a6954d465
Leo Conforti <leo@leoconforti.us>
2026-05-20 11:09:39 -0500

    revert tests

--- packages/platform/test/stream.test.ts
+++ packages/platform/test/stream.test.ts
@@ -1,33 +1,27 @@
-// import { Data, Effect, Stream } from "effect";
+import { Data, Effect, Stream } from "effect";
 
-// import { describe, expect, it } from "@effect/vitest";
+import { describe, expect, it } from "@effect/vitest";
 
-// class ReadError extends Data.TaggedError("ReadError")<{ message: string }> {}
+class ReadError extends Data.TaggedError("ReadError")<{ message: string }> {}
 
-// import * as FridaStream from "@efffrida/platform/Stream";
-
-// describe("stream tests", () => {
-//     it.effect("roundtrips stream -> inputstream -> stream", () =>
-//         Effect.gen(function* () {
-//             const originalData = new Uint8Array([1, 2, 3, 4, 5]);
-//             const originalStream = Stream.make(originalData);
-//             const inputStream = FridaStream.toInputStreamNever(originalStream);
-
-//             const reconstructedStream = FridaStream.fromInputStream(
-//                 () => inputStream,
-//                 (e) => new ReadError({ message: `Read error: ${e}` }),
-//                 { chunkSize: 1 }
-//             );
-
-//             const result = yield* Stream.runCollect(reconstructedStream);
-//             const allBytes = result.flatMap((arr) => Array.from(arr));
-//             expect(allBytes).toEqual([1, 2, 3, 4, 5]);
-//         })
-//     );
-// });
-
-import { describe, it } from "@effect/vitest";
+import * as FridaStream from "@efffrida/platform/Stream";
 
 describe("stream tests", () => {
-    it.todo("todo", () => {});
+    it.effect("roundtrips stream -> inputstream -> stream", () =>
+        Effect.gen(function* () {
+            const originalData = new Uint8Array([1, 2, 3, 4, 5]);
+            const originalStream = Stream.make(originalData);
+            const inputStream = FridaStream.toInputStreamNever(originalStream);
+
+            const reconstructedStream = FridaStream.fromInputStream(
+                () => inputStream,
+                (e) => new ReadError({ message: `Read error: ${e}` }),
+                { chunkSize: 1 }
+            );
+
+            const result = yield* Stream.runCollect(reconstructedStream);
+            const allBytes = result.flatMap((arr) => Array.from(arr));
+            expect(allBytes).toEqual([1, 2, 3, 4, 5]);
+        })
+    );
 });
