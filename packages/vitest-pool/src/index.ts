b683a2f928c8b3d032e768f1533d31446a1dfa76
Leo Conforti <leo@leoconforti.us>
2026-05-20 11:23:45 -0500

    Revert "assign random name"
    This reverts commit 2e4f857ce93c8b4e12de93d60d4a21f96ef1323e.

--- packages/vitest-pool/src/index.ts
+++ packages/vitest-pool/src/index.ts
@@ -81,7 +81,7 @@ const ConfigSchema = Schema.Struct({
  */
 export class FridaPoolWorker implements VitestNode.PoolWorker {
     readonly agentTemplatePath = new URL("../frida/agent.ts", import.meta.url);
-    readonly name = `frida-pool${Math.floor(Math.random() * 10000)}`;
+    readonly name = "frida-pool";
 
     private readonly customOptions: Schema.Schema.Type<typeof ConfigSchema>;
 
