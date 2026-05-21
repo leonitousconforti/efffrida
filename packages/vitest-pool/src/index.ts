22ec342c649d812631578b96a01588df7806cb3a
Leo Conforti <leo@leoconforti.us>
2026-05-21 13:00:32 -0500

    revert

--- packages/vitest-pool/src/index.ts
+++ packages/vitest-pool/src/index.ts
@@ -25,10 +25,6 @@ import * as FridaSession from "@efffrida/frida-tools/FridaSession";
 import * as Flatted from "flatted";
 import * as Frida from "frida";
 
-process.once("beforeExit", async () => {
-    await Frida.getDeviceManager().close();
-});
-
 // First, pick your device
 const DeviceSchema = Schema.Union([
     Schema.Struct({
