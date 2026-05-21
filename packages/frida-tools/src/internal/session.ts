87c26cde17136252995c68d6a6033b90ad0d096d
Leo Conforti <leo@leoconforti.us>
2026-05-21 12:08:54 -0500

    test?

--- packages/frida-tools/src/internal/session.ts
+++ packages/frida-tools/src/internal/session.ts
@@ -103,7 +103,9 @@ export const spawn = (
             Effect.promise((signal) => {
                 const cancellable = new Frida.Cancellable();
                 signal.onabort = () => cancellable.cancel();
-                return device.kill(pid, cancellable);
+                console.log(`Killing process with PID ${pid}...`);
+                return Promise.resolve();
+                // return device.kill(pid, cancellable);
             })
         );
 
