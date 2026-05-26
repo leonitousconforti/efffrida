8543f41cd6d5a0281aed4493f8f2105a680fae9e
Leo Conforti <leo@leoconforti.us>
2026-05-26 16:38:08 -0500

    formatting

--- packages/create-efffrida-app/src/bin.ts
+++ packages/create-efffrida-app/src/bin.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-import * as NodeHttpClient from "@effect/platform-node/NodeHttpClient";
-import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
-import * as NodeServices from "@effect/platform-node/NodeServices";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 import * as Logger from "effect/Logger";
 import * as References from "effect/References";
+
+import * as NodeHttpClient from "@effect/platform-node/NodeHttpClient";
+import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
+import * as NodeServices from "@effect/platform-node/NodeServices";
 
 import { cli } from "./Cli.js";
 import { GitHub } from "./GitHub.js";
@@ -17,13 +18,10 @@ const MainLive = GitHub.Default.pipe(
         Layer.mergeAll(
             Logger.layer([PrefixLogger]),
             Layer.succeed(References.MinimumLogLevel, "Info"),
-            NodeServices.layer,
-            NodeHttpClient.layerUndici
+            NodeHttpClient.layerFetch,
+            NodeServices.layer
         )
     )
 );
 
-cli.pipe(
-    Effect.provide(MainLive),
-    NodeRuntime.runMain({ disableErrorReporting: true })
-);
+cli.pipe(Effect.provide(MainLive), NodeRuntime.runMain({ disableErrorReporting: true }));
