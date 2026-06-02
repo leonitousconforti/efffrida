a984073604a60cfcef579515e9a1389862eed5ae
Leo Conforti <leo@leoconforti.us>
2026-06-02 14:38:11 -0500

    update tests

--- templates/agent-only/test/index.test.ts
+++ templates/agent-only/test/index.test.ts
@@ -7,7 +7,7 @@ describe("agent tests", () => {
     it.effect("should have the correct frida version", () =>
         Effect.gen(function* () {
             const version = yield* FridaVersion.use((frida) => frida.version);
-            expect(version).toBe("17.9.11");
+            expect(version).toBe("17.10.1");
         }).pipe(Effect.provide(FridaVersion.default))
     );
 });
