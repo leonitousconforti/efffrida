207c36c1f3bf9098b830954f42ca853304bef5fa
Leo Conforti <leo@leoconforti.us>
2026-06-08 14:38:18 -0500

    fix tests

--- packages/vitest-pool/test/spec2.test.ts
+++ packages/vitest-pool/test/spec2.test.ts
@@ -9,6 +9,6 @@ describe("vitest-pool spec2", () => {
     });
 
     it("can access Frida APIs", () => {
-        expect(Frida.version).toBe("17.10.1");
+        expect(Frida.version).toBe("17.11.0");
     });
 });
