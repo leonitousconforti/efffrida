1de9bbe10aa30e18659622e0f3d73ebe51dbb91c
Leo Conforti <leoconforti@ltgk.net>
2026-06-17 11:22:53 -0500

    Frida 17.14.1 (#83)
    * Frida 17.14.1
    
    * Fix tests

--- packages/vitest-pool/test/effect.test.ts
+++ packages/vitest-pool/test/effect.test.ts
@@ -14,7 +14,7 @@ describe("vitest-pool effect", () => {
 
     it.effect("can access Frida APIs", () =>
         Effect.gen(function* () {
-            expect(Frida.version).toBe("17.12.0");
+            expect(Frida.version).toBe("17.14.1");
         })
     );
 });
