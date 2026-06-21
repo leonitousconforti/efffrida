0d65c05585ee87e1131af9ba2f268609183c89f5
Leo Conforti <leo@leoconforti.us>
2026-06-21 15:19:39 -0500

    tests

--- packages/vitest-pool/test/spec1.test.ts
+++ packages/vitest-pool/test/spec1.test.ts
@@ -9,6 +9,6 @@ describe("vitest-pool spec1", () => {
     });
 
     it("can access Frida APIs", () => {
-        expect(Frida.version).toBe("17.14.1");
+        expect(Frida.version).toBe("17.15.1");
     });
 });
