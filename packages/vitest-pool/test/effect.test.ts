c3050c194be958975a1cb7db5fd7109ea175f991
Leo Conforti <leo@leoconforti.us>
2026-07-21 16:49:36 -0500

    fix tests

--- packages/vitest-pool/test/effect.test.ts
+++ packages/vitest-pool/test/effect.test.ts
@@ -14,7 +14,7 @@ describe("vitest-pool effect", () => {
 
     it.effect("can access Frida APIs", () =>
         Effect.gen(function* () {
-            expect(Frida.version).toBe("17.15.5");
+            expect(Frida.version).toBe("17.16.3");
         })
     );
 });
