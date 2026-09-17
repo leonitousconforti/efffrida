21131d056a552f88deffa1e7230e6c1b7b158fe5
Leo Conforti <leo@leoconforti.us>
2026-09-17 11:31:55 -0500

    bump frida version

--- packages/vitest-pool/test/effect.test.ts
+++ packages/vitest-pool/test/effect.test.ts
@@ -14,7 +14,7 @@ describe("vitest-pool effect", () => {
 
     it.effect("can access Frida APIs", () =>
         Effect.gen(function* () {
-            expect(Frida.version).toBe("17.17.0");
+            expect(Frida.version).toBe("17.18.0");
         })
     );
 });
--- packages/vitest-pool/test/spec2.test.ts
+++ packages/vitest-pool/test/spec2.test.ts
@@ -9,6 +9,6 @@ describe("vitest-pool spec2", () => {
     });
 
     it("can access Frida APIs", () => {
-        expect(Frida.version).toBe("17.17.0");
+        expect(Frida.version).toBe("17.18.0");
     });
 });
